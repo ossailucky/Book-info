16891e1c50e0f6550b1d3c5441e77ed844e32034
Ossai Lucky <luckyblaqy@gmail.com>
2023-01-25 18:35:56 +0100

    scoffolded the app

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+const app = express();
+
+
+app.use('/graphql',graphqlHTTP({
+
+}))
+
+app.listen(4000, ()=>{
+    console.log("now listening for requests on port 4000");
+});
