d9aefe217be16c9627c1af0ea73cd2aa5309567e
Ossai Lucky <luckyblaqy@gmail.com>
2023-01-26 22:41:44 +0100

    create the root query

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,4 @@
-import graphql from "graphql";
+import graphql, { GraphQLSchema } from "graphql";
 
 const { GraphQLObjectType, GraphQLString } = graphql;
 
@@ -9,4 +9,21 @@ const BookType = new GraphQLObjectType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book:{
+            type: BookType,
+            args: {id:{type: GraphQLString}},
+            resolve(parent,args){
+                // code to get data from db / other source
+            }
+        }
+    }
+});
+
+export default new GraphQLSchema({
+    query:RootQuery
+});
