d874f6014d06c1837962c2963d361888c6394910
Ossai Lucky <luckyblaqy@gmail.com>
2023-01-29 16:04:56 +0100

    add more books and resolve author schema to fetch list of books by author

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 import graphql from "graphql";
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt  } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt,  GraphQLList   } = graphql;
 import _ from "lodash"
 
 
@@ -8,12 +8,18 @@ var books = [
     {name:"Name of the Wind",genre:"Fantasy", id:"1", authorId: "1"},
     {name:"The Final Empire",genre:"Fantasy", id:"2", authorId: "2"},
     {name:"The Long Earth",genre:"Sci-Fi", id:"3", authorId: "3"},
+    {name:"The Hero of Ages",genre:"Fantasy", id:"4", authorId: "2"},
+    {name:"The Colour of Magic",genre:"Fantasy", id:"5", authorId: "3"},
+    {name:"The Light Fanastic",genre:"Fantasy", id:"6", authorId: "3"},
+
+
+
 ];
 
 var authors = [
     {name: "Patrick Rothfuss", age: 44, id: "1"},
     {name: "Brandon Sanderson", age: 42, id: "2"},
-    {name: "Terry Prachett", age: 66, id: "2"}
+    {name: "Terry Prachett", age: 66, id: "3"}
 
 
 ];
@@ -38,7 +44,13 @@ const AuthorType = new GraphQLObjectType({
     fields: ()=>({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                return _.filter(books,{authorId:parent.id});
+            }
+        }
     })
 });
 
